5fd9d60aa29cae83991a3debc68f95acca14c501
Lucas Vieira <lnvieira@paschoalotto.com.br>
2021-10-02 18:44:36 -0300

    Completed module

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
 using StarChart.Data;
+using StarChart.Models;
 
 namespace StarChart.Controllers
 {
@@ -12,6 +14,48 @@ namespace StarChart.Controllers
         public CelestialObjectController(ApplicationDbContext context)
         {
             _context = context;
+        }
+
+        [HttpGet("{id:int}", Name = "GetById")]
+        public IActionResult GetById(int id)
+        {
+            var celestialObject = _context.CelestialObjects.Find(id);
+
+            if (celestialObject is null)
+                return NotFound();
+
+            celestialObject.Satellites.Add(celestialObject);
+
+            return Ok(celestialObject);
+        }
+
+        [HttpGet("{name}", Name = "GetByName")]
+        public IActionResult GetByName(string name)
+        {
+            var celestialObjectList = _context.CelestialObjects.Where(x => x.Name == name);
+
+            if (!celestialObjectList.Any())
+                return NotFound();
+
+            foreach (var item in celestialObjectList)
+            {
+                item.Satellites.Add(item);
+            }
+
+            return Ok(celestialObjectList);
+        }
+
+        [HttpGet()]
+        public IActionResult GetAll()
+        {
+            var celestialObjectList = _context.CelestialObjects;           
+
+            foreach (var item in celestialObjectList)
+            {
+                item.Satellites.Add(item);
+            }
+
+            return Ok(celestialObjectList);
         }
     }
 }
